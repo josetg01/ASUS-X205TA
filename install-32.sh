716765aea50f60ab8fe21ffb732cd4cfdb11da24
josetg01 <35702013+josetg01@users.noreply.github.com>
2019-11-16 11:02:38 +0100

    Create install-32.sh

---
+++ install-32.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Download firmware file and install it
+wget http://lopaka.github.io/files/instructions/brcmfmac43340-sdio.txt -O /lib/firmware/brcm/brcmfmac43340-sdio.txt
+wget http://lopaka.github.io/files/instructions/BCM43341B0.hcd -O /lib/firmware/brcm/BCM43341B0.hcd
+
+# Create systemd service file
+cat >/etc/systemd/system/btattach.service <<EOL
+[Unit]
+Description=Btattach
+[Service]
+Type=simple
+ExecStart=/usr/bin/btattach --bredr /dev/ttyS1 -P bcm
+ExecStop=/usr/bin/killall btattach
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Enable service
+systemctl enable btattach
+
+# Install Sound
+wget http://x205ta.myftp.org:1337/kernel/kernel-32bit-5.0.1-sound-41.tar
+tar xf kernel-32bit-5.0.1-sound-41.tar
+sudo ./install-sound-kernel.sh -v 
+sudo apt install pavucontrol
