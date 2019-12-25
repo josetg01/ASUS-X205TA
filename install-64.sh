39d32708f86d3a7015dccd72e2a844848c737bd7
josetg01 <35702013+josetg01@users.noreply.github.com>
2019-12-25 19:11:47 +0100

    Update install-64.sh

--- install-64.sh
+++ install-64.sh
@@ -19,9 +19,3 @@ EOL
 
 # Enable service
 systemctl enable btattach
-
-# Install Sound
-wget http://x205ta.myftp.org:1337/kernel/kernel-64bit-5.0.1-sound-41.tar
-tar xf kernel-64bit-5.0.1-sound-41.tar
-sudo ./install-sound-kernel.sh -v 
-sudo apt install pavucontrol
