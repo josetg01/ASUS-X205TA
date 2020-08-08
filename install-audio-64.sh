de1fa2ad9cac815859eeb9ec26245497775ed139
josetg01 <35702013+josetg01@users.noreply.github.com>
2020-08-08 12:40:09 +0200

    Update install-audio-64.sh

--- install-audio-64.sh
+++ install-audio-64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://sourceforge.net/projects/distros-x205ta/files/kernel/kernel-64bit-sound.tar/download
-tar xf kernel-64bit-sound.tar
+wget https://github.com/josetg01/ASUS-X205TA/raw/master/Drivers/Audio/kernel-64bit-5.3.7-sound-43.tar
+tar xf kernel-64bit-5.3.7-sound-43.tar
 sudo ./install-sound-kernel.sh -v 
 sudo apt install pavucontrol
